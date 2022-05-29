3a12aadfdff9bff45e6c8c5aeea205cdd8d58d15
ovge <73331192+qisor@users.noreply.github.com>
2022-05-29 17:01:32 +0800

    add shell code

--- shell/command/spider.sh
+++ shell/command/spider.sh
@@ -1,21 +1,62 @@
 #!bash
 
-#for ((i=0;i<10;i++))
-#do
-#curl https://lmg.jj20.com/up/allimg/1115/032022105050/220320105050-1-1200.jpg > pic.jpg
-#curl https://lmg.jj20.com/up/allimg/1115/032022105050/220320105050-2-1200.jpg > pic2.jpg
-#done
-#line=`curl https://m.jj20.com/bz/hhzw/xhtx/389394_2.html | grep 'info_pic'`
-#link=grep 'src="(//.*jpg)"' ${line}
-#
-#echo line:${line}
-#echo link:${link}
-str='<div class="info_pic"><a href='389394_3.html'><img src="//lmg.jj20.com/up/allimg/1115/032022105050/220320105050-2-1200.jpg" alt="□□ǣţ□□ͼƬ□□ȫ□□ͼ"/></a></div> '
+alllink=()
+#root_link=$(curl https://m.jj20.com/bz/nxxz/shxz/ |grep 'm.jj20.com')
+root_link=$(curl https://www.jj20.com/bz/nxxz/shxz/list_62_cc_11.html | grep 'bz/nxxz/shxz')
+echo root_link:${root_link}
+c=0
+for i in $root_link; do
+    if [[ $i =~ "bz/nxxz/shxz/"[0-9]+\.html\"$ ]]; then
+        echo $c:$i
+        # shellcheck disable=SC2219
+        tmp=${i:6}
+        alllink[$c]=https://www.jj20.com/${tmp//\"/ }
+        let c++
+    fi
+done
+
+echo ${alllink[*]}
+
+# shellcheck disable=SC2068
+file=1
+for enery_link in ${alllink[@]}; do
+    {
+        line=$(curl ${enery_link} | grep 'bigImg')
+        echo line:"$line"
+
+        link1=$(echo $line | grep -o 'lmg.*jpg')
+        for ((i = 1; i < 10; i++)); do
+            {
+                let file ++
+                link="https://${link1//-1-/-$i-}"
+                echo $link
+                curl $link > $file$i.jpg
+            } &
+        done
+    }
+    #link=grep 'src="(//.*jpg)"' ${line}
+done
+
+wait
+
+exit 0
+
+str=$line
 echo str:$str
-link1=`echo $str | grep -o 'lmg.*jpg'`
-link2=`echo $str | sed -n 's#.*\(lmg.*jpg\).*#\1#p'`
-link3=`echo $str | awk -F"[\"]" '/lmg.*jpg/{print $4}'`
+link1=$(echo $str | grep -o 'lmg.*jpg')
+link2=$(echo $str | sed -n 's#.*\(lmg.*jpg\).*#\1#p')
+link3=$(echo $str | awk -F"[\"]" '/lmg.*jpg/{print $4}')
 
 echo link1:$link1
 echo link2:$link2
 echo link3:$link3
+
+for ((i = 1; i < 10; i++)); do
+    {
+        link="https://${link1//-1-/-$i-}"
+        echo $link
+        curl $link > $i.jpg
+    } &
+done
+
+wait
