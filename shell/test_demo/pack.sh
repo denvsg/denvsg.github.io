4ca351a233cfae14e139df79b7beaa08aba0567b
ovge <73331192+qisor@users.noreply.github.com>
2022-06-11 16:38:48 +0800

    fix: modify shell code

--- shell/test_demo/pack.sh
+++ shell/test_demo/pack.sh
@@ -1,10 +1,18 @@
 #!bash
 
-set -x
-
+# set -x
+function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
 if ! node -v &> /dev/null ;then
     echo -e "\e[31m 请配置node和npm环境变量 \e[0m"
     exit 9
+else
+    # node版本要求： v14.18.3以及以上版本
+    node_version=`node -v`
+    if version_gt ${node_version:1} 14.18.3; then
+        echo " ${node_version:1} is greater than 14.18.3" &> /dev/null 
+    else
+        echo -e "\e[31m Please upgrade nodejs version\n\e[0m"
+    fi
 fi
 
 read -p "请输入工程根目录名:" root_dir
@@ -27,7 +35,7 @@ fi
 
 echo "开始编译打包"
 
-if ! grep 'hvigor' package.json &> /dev/null ;then
+if ! grep 'hvigor' package.json &> /dev/null;then
     compile_cmd=' "scripts": {\n    "build": "hvigor clean assembleHap"\n  },'
     sed -i "/version/ i\ ${compile_cmd}" package.json
 fi
@@ -42,15 +50,15 @@ fi
 
 pages='/entry/build/default/intermediates/assets/default/ets/pages'
 node_modules_path='/entry/build/default/intermediates/assets/default/node_modules'
-page_path=${root_dir}${pages}
-node_kit_path=${root_dir}${node_modules_path}
+page_compile_path=${root_dir}${pages}
+node_modules_compile_path=${root_dir}${node_modules_path}
 if [ ${status} -eq 0 ];then
-    if find ${page_path} -name index.js ;then
+    if find ${page_compile_path} -name index.js &> /dev/null;then
         echo "pages compiler success"
     else
         echo -e "\e[31m pages compiler fail\e[0m"
     fi
-    if find ${node_kit_path} -name "*.js" ;then
+    if find ${node_modules_compile_path} -name "*.js" &> /dev/null;then
         echo "node_modules compiler success"
     else
         echo -e "\e[31m node_modules compiler fail\e[0m"
